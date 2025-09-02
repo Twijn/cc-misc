05f7b19b5aa02ca3c8f4361ac12355fefe814c94
Tyler Twining <twijn@twijn.net>
2025-09-02 12:37:17 -0500

    wip recipes & prices

---
+++ brewery/prices.lua
@@ -0,0 +1,70 @@
+return {
+    -- Water bottles
+    {
+        name = "minecraft:potion",
+        potion = "minecraft:water",
+        cost = 1 / 300, -- = 0.003 repeating (0.01 = 3 potions)
+    },
+    ---------------
+    -- Modifiers --
+    ---------------
+    {
+        name = "minecraft:nether_wart",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:redstone",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:glowstone_dust",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:fermented_spider_eye",
+        cost = 0.02,
+    },
+    {
+        name = "minecraft:gunpowder",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:dragon_breath",
+        cost = 0.05,
+    },
+    -----------------
+    -- Ingredients --
+    -----------------
+    {
+        name = "minecraft:sugar",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:golden_carrot",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:rabbit_foot",
+        cost = 0.20,
+    },
+    {
+        name = "minecraft:magma_cream",
+        cost = 0.02,
+    },
+    {
+        name = "minecraft:turtle_helmet",
+        cost = 0.50,
+    },
+    {
+        name = "minecraft:pufferfish",
+        cost = 0.20,
+    },
+    {
+        name = "minecraft:spider_eye",
+        cost = 0.01,
+    },
+    {
+        name = "minecraft:ghast_tear",
+        cost = 0.05,
+    },
+}
