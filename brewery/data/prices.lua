c0e8c62bbb87bb31307f8c37e50a6eef61297123
Tyler Twining <twijn@twijn.net>
2025-09-02 14:30:50 -0500

    feat: Add display names to prices

--- brewery/data/prices.lua
+++ brewery/data/prices.lua
@@ -2,6 +2,7 @@ return {
     -- Water bottles
     {
         name = "minecraft:potion",
+        displayName = "Water Bottle",
         potion = "minecraft:water",
         cost = 1 / 300, -- = 0.003 repeating (0.01 = 3 potions)
     },
@@ -10,26 +11,32 @@ return {
     ---------------
     {
         name = "minecraft:nether_wart",
+        displayName = "Nether Wart",
         cost = 0.01,
     },
     {
         name = "minecraft:redstone",
+        displayName = "Redstone",
         cost = 0.01,
     },
     {
         name = "minecraft:glowstone_dust",
+        displayName = "Glowstone Dust",
         cost = 0.01,
     },
     {
         name = "minecraft:fermented_spider_eye",
+        displayName = "Fmtd Spider Eye",
         cost = 0.02,
     },
     {
         name = "minecraft:gunpowder",
+        displayName = "Gunpowder",
         cost = 0.01,
     },
     {
         name = "minecraft:dragon_breath",
+        displayName = "Dragon Breath",
         cost = 0.05,
     },
     -----------------
@@ -37,46 +44,57 @@ return {
     -----------------
     {
         name = "minecraft:sugar",
+        displayName = "Sugar",
         cost = 0.01,
     },
     {
         name = "minecraft:golden_carrot",
+        displayName = "Golden Carrot",
         cost = 0.01,
     },
     {
         name = "minecraft:rabbit_foot",
+        displayName = "Rabbit Foot",
         cost = 0.20,
     },
     {
         name = "minecraft:magma_cream",
+        displayName = "Magma Cream",
         cost = 0.02,
     },
     {
         name = "minecraft:turtle_helmet",
+        displayName = "Turtle Helmet",
         cost = 0.50,
     },
     {
         name = "minecraft:pufferfish",
+        displayName = "Pufferfish",
         cost = 0.20,
     },
     {
         name = "minecraft:spider_eye",
+        displayName = "Spider Eye",
         cost = 0.01,
     },
     {
         name = "minecraft:ghast_tear",
+        displayName = "Ghast Tear",
         cost = 0.05,
     },
     {
         name = "minecraft:glistering_melon_slice",
+        displayName = "Gstng Melon Slice",
         cost = 0.01,
     },
     {
         name = "minecraft:blaze_powder",
+        displayName = "Blaze Powder",
         cost = 0.01,
     },
     {
         name = "minecraft:phantom_membrane",
+        displayName = "Phtm Membrane",
         cost = 0.10,
     },
 }
